6228ef552c0ca5a20294a72ffe33707807d09ef4
Taras Suhovenko <tsuhovenko@Tarass-MacBook-Pro-2.local>
2018-05-28 00:36:49 +0300

    initial commit

---
+++ contracts/Voting.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.4.22;
+
+contract Voting {
+    mapping (bytes32 => uint8) public votesReceived;
+}
---
+++ truffle.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  // Mnemonic: candy maple cake sugar pudding cream honey rich smooth crumble sweet treat
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  }
+};
